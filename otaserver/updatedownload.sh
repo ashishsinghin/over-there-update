567b148ed86a7ea65cf64b85574312ef9da61200
Ashish Singh <ashishsingh21798@gmail.com>
2024-09-30 10:27:30 +0530

    script file added for check and download

---
+++ otaserver/updatedownload.sh
@@ -0,0 +1,5 @@
+# Fetch the headers and extract the filename
+filename=$(curl -s -D - "http://localhost:8080/download?version=1.2.0" -o /dev/null | grep -i "^Content-Disposition:" | sed -n 's/.*filename="\([^"]*\)".*/\1/p')
+
+# Download the file using the extracted filename
+curl -o "$filename" "http://localhost:8080/download?version=1.2.0"
